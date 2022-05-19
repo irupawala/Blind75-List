ab7273103550bcba875e0ca4c6d25bae5bdc4ea1
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-05-19 11:58:44 -0700

    Create [65] Invert Binary Tree.py

---
+++ [65] Invert Binary Tree.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+# Definition for a binary tree node.
+#class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+        
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        
+        # swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+    
+'''
+Time Complexity - O(Height)
+Space Complaxity - O(1)
+'''
