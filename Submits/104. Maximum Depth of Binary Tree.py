aad27f2f2199c946610c87fc4f17b1afd6c9ef3d
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-05-19 16:28:04 -0700

    Update 104. Maximum Depth of Binary Tree.py

--- Submits/104. Maximum Depth of Binary Tree.py
+++ Submits/104. Maximum Depth of Binary Tree.py
@@ -4,6 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# BFS
+
+from collections import deque
+
+# Ibrahim Iterative
+'''
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
@@ -18,11 +24,53 @@ class Solution:
                 max_depth = max(InOrderTraversal(node.left, depth+1, max_depth), max_depth)
             if node.right:
                 max_depth = max(InOrderTraversal(node.right, depth+1, max_depth), max_depth)        
-            return max(InOrderTraversal(node.left, depth+1, max_depth), max_depth)
+            return max_depth
         
         return InOrderTraversal(root, depth, max_depth)
+'''    
+
+# BFS
+'''
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        qmain = deque()
+        max_depth = 0
+        qmain.append(root)
+        
+        while qmain:   
+            for i in range(len(qmain)):
+                node = qmain.popleft()
+                if node.left: qmain.append(node.left)
+                if node.right: qmain.append(node.right)
+            max_depth += 1
+        return max_depth
+'''
+    
+    
+# ITERATIVE DFS   
+'''
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        stack = [[root, 1]]
+        res = 0
+        
+        while stack:
+            node, depth = stack.pop()
+        
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res    
+'''  
+    
     
 '''
 Time Complexity - log(n)
 Space Complexity - O(1)
 '''
+
+
