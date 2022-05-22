085ed5da3505b5f9534159b1b20c1a636a5edcca
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-05-21 19:50:56 -0700

    Update 190. Reverse Bits.py

--- Submits/190. Reverse Bits.py
+++ Submits/190. Reverse Bits.py
@@ -5,7 +5,18 @@ class Solution:
             bit = (n >> i) & 1
             res = res | (bit << (31 - i))
         return res
-    
+ 
+
+'''
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        int_n = bin(n)[2:]
+        int_32 = "%32s" %(int_n)
+        int_32 = int_32.replace(" ", "0")
+        return(int(int_32[::-1], 2))
+'''    
+
+
 '''
 Time Complexity - O(32)
 Space Complexity - O(1)
