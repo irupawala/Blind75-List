d0689c33c99d0f5cbfe5b7605f6777bcf4272aca
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-05-23 15:38:40 -0700

    Update 49. Group Anagrams.py

--- Submits/49. Group Anagrams.py
+++ Submits/49. Group Anagrams.py
@@ -1,20 +1,40 @@
-from heapq import *
+'''
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 1: return [strs] 
+        ans = {}
+        
+        for i in strs:
+            dict_key = "".join(map(str, sorted(i)))
+            if dict_key not in ans: ans[dict_key] = [i]
+            else: ans[dict_key].append(i)
+                
+        return ans.values()
+'''   
+
+'''
+Time Complexity - O(len(strs) * mlogm)
+Space Complexity - O(len(strs))
+'''
+
+
+# Linear time algorithm by Neetcode
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        set_nums = set(nums)
-        ans = []
-        heap = []
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
         
-        for num in set_nums: 
-            heappush(heap, [-nums.count(num), num])
-            
-        for i in range(k):
-            ans.append(heappop(heap)[1])
-            
-        return ans
-        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
+
+
 '''
-Time Complexity - O(unique_nums*log(unique_nums) + O(k*unique_nums)
-Space Complexity - O(unique_nums)
+Time Complexity - O(len(str) * m * 26)
+Space Complexity - O(len(str) * m)
 '''
+
+
