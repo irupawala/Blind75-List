29ee09c99312bce296bacea414c5c64a4d3090c4
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-06-11 22:00:31 -0700

    Update 198. House Robber.py

--- Submits/198. House Robber.py
+++ Submits/198. House Robber.py
@@ -10,3 +10,21 @@ class Solution:
             dp[i] = nums[i] + max(dp[:i-1])
             
         return max(dp)
+
+'''
+Time Complexity - O(n)
+Space Complexity - O(n)
+'''
+
+ #NeetCode Solution  
+ '''   
+ class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
+   '''
