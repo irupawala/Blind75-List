cb18b90c5be664ef1d0c21ebabd002f3dc0fc864
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-06-14 15:50:37 -0700

    Create 62. Unique Paths.py

---
+++ Submits/62. Unique Paths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [ [0 for i in range(n)] for _ in range(m)]
+        for i in range(n): dp[0][i] = 1
+        for j in range(m): dp[j][0] = 1
+
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[j][i] += dp[j-1][i]
+                dp[j][i] += dp[j][i-1]
+                
+        return dp[m-1][n-1]
+
+S = Solution()
+S.uniquePaths(3, 2)
+
+'''
+Time Complexity - O(n*m)
+Space Complexity - O(n*m)
+'''
