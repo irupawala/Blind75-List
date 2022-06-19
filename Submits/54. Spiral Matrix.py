2f07f9e60576133fbbc81239e6b326ddf0514f26
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-06-18 17:00:33 -0700

    Update 54. Spiral Matrix.py

--- Submits/54. Spiral Matrix.py
+++ Submits/54. Spiral Matrix.py
@@ -29,3 +29,14 @@ class Solution:
             top += 1
         
         return result
+                
+                
+'''              
+S = Solution()
+print(S.spiralOrder([[1,2,3,4,5,6], [7,8,9,10,11,12], [13,14,15,16,17,18], [19,20,21,22,23,24], [25,26,27,28,29,30]]))
+'''  
+
+'''
+Time Complexity - O(n+m)
+Space Complexity - O(1)
+'''
