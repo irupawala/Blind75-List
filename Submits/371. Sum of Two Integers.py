8924451bd7cb2d9cc27ec11293cf2be3ec41b70e
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-06-19 12:36:54 -0700

    Update 371. Sum of Two Integers.py

--- Submits/371. Sum of Two Integers.py
+++ Submits/371. Sum of Two Integers.py
@@ -19,3 +19,20 @@ class Solution:
   Time Complexity - O(1) because number won't grow any bigger
   Space Complexity - O(1)
   '''
+
+
+'''
+JAVA Solution
+
+class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
+}
+'''
