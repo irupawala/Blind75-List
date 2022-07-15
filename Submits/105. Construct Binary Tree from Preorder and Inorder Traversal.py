ec5e7b8c256beba77fa61dfaf93e3087e6b1ec1a
Ibrahim Rupawala <88699567+irupawala@users.noreply.github.com>
2022-07-14 17:09:44 -0700

    Create 105. Construct Binary Tree from Preorder and Inorder Traversal.py

---
+++ Submits/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,63 @@
+# LeetCode Link - https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def array_to_tree(left, right):
+            nonlocal preorder_index
+            # if there are no elements to construct the tree
+            if left > right: return None
+
+            # select the preorder_index element as the root and increment it
+            root_value = preorder[preorder_index]
+            root = TreeNode(root_value)
+
+
+            preorder_index += 1
+
+            # build left and right subtree
+            # excluding inorder_index_map[root_value] element because it's the root
+            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
+            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
+
+            return root
+
+        preorder_index = 0
+
+        # build a hashmap to store value -> its index relations
+        inorder_index_map = {}
+        for index, value in enumerate(inorder):
+            inorder_index_map[value] = index
+
+        return array_to_tree(0, len(preorder) - 1)        
+
+# Without HashMap
+'''
+class Solution:
+  def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder or not inorder:
+            return None       
+        
+        root = TreeNode(preorder[0])
+        m = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:m+1], inorder[:m])
+        root.right = self.buildTree(preorder[m+1:], inorder[m+1:])
+        return root
+    
+'''
+
+
+'''
+Time Complexity - O(N)
+Space Complexity - O(N) # The size of the implicit system stack used by recursion calls depends on the height of the tree, which is O(N) in the worst case and O(logN) on average. Taking both into consideration, the space complexity is O(N).
+'''
